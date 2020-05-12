e2c004aaf1ca67edcd95eea2f372f890db9fc02e
shengxun <fanzhengchen@gmail.com>
2020-05-12 14:13:44 +0800

    controller

--- sse/src/main/java/com/qunhe/instdeco/MainApplication.java
+++ sse/src/main/java/com/qunhe/instdeco/MainApplication.java
@@ -2,10 +2,13 @@ package com.qunhe.instdeco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author shengxun
  */
+@EnableScheduling
 @SpringBootApplication
 public class MainApplication {
 
--- sse/src/main/java/com/qunhe/instdeco/RestController.java
+++ sse/src/main/java/com/qunhe/instdeco/RestController.java
@@ -1,39 +1,69 @@
 package com.qunhe.instdeco;
 
+import io.netty.util.internal.ConcurrentSet;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author shengxun
  */
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-    private SseEmitter event = new SseEmitter();
 
-    private Thread mThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            for (; ; ) {
-                try {
-                    event.send(SseEmitter.event().data("sse data"));
-                    LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(5));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    });
+    private Set<SseEmitter> sseEmitters = new HashSet<>();
+
+    private Lock mLock = new ReentrantLock();
+
+    private int messageCount = 0;
 
     @GetMapping(value = "/sse", produces = "text/event-stream;charset=UTF-8")
     public SseEmitter sseResponse() throws Exception {
-
-        event.send(SseEmitter.event().reconnectTime(10000L).id("123"));
-        if (mThread.getState() == Thread.State.NEW) {
-            mThread.start();
+        final SseEmitter sseEmitter = new SseEmitter();
+        mLock.lock();
+        try {
+            sseEmitters.add(sseEmitter);
+        } finally {
+            mLock.unlock();
         }
-        return event;
+        return sseEmitter;
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void scheduledMsgEmitter() throws IOException {
+        mLock.lock();
+        try {
+            System.out.println("xx");
+            if (!sseEmitters.isEmpty()) {
+                ++messageCount;
+            } else {
+                System.out.println("No active Emitters ");
+            }
+
+            System.out.println("Sent Messages : " + messageCount);
+            sseEmitters.forEach(emitter -> {
+                if (emitter != null) {
+                    try {
+                        System.out.println("Timeout : " + emitter.getTimeout());
+                        emitter.send("MessageCounter : " + messageCount);
+                        //emitter.complete();
+                        //sseEmitters.remove(emitter);
+                    } catch (Exception e) {
+                        sseEmitters.remove(emitter);
+                    }
+                }
+            });
+        } finally {
+            mLock.unlock();
+        }
     }
 }
